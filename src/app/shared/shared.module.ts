6fb135dab1ba4ce78c95ce43474e41b39407a4df
Diogo Castro <ctw00156@criticaltechworks.com>
2018-10-03 15:01:26 +0100

    initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+@NgModule({
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  declarations: [],
+  exports: [CommonModule, ReactiveFormsModule, RouterModule],
+  providers: []
+})
+export class SharedModule {}
